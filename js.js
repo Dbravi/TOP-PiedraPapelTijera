36646c4dd007acb7fad5fad404b9dd9d506a0d9c
Dbravi <damianbravi@gmail.com>
2022-05-17 14:03:26 -0300

    Add score system

--- js.js
+++ js.js
@@ -1,39 +1,61 @@
-const papel="papel"
-const piedra="piedra"
-const tijera="tijera"
+const papel = "papel"
+const piedra = "piedra"
+const tijera = "tijera"
+const palabras = ["piedra", "papel", "tijera"]
 
-const palabras= ["piedra","papel", "tijera"]
-function computerPlay() {
-    let resultado= Math.floor(Math.random() * palabras.length);
-    console.log(palabras[resultado]) 
-  }
-
-// computerPlay()
-
-const singleRound= function (playerSelection, computerSelection) {
-    playerSelection=playerSelection.toLowerCase()
-if (playerSelection=== computerSelection) {
-    return console.log ("empate")
-}
-else if(playerSelection === papel && computerSelection === "piedra") {
-   console.log("papel gana")
-}
-else if(playerSelection === papel && computerSelection === "tijera") {
-    console.log("tijera gana")
-}
-else if(playerSelection === piedra && computerSelection === "tijera") {
-    console.log("piedra gana")
-}
-else if(playerSelection === piedra && computerSelection === "papel") {
-    console.log("tijera gana")
-}
-else if(playerSelection === tijera && computerSelection === "papel") {
-    console.log("tijera gana")
-}
-else if(playerSelection === tijera && computerSelection === "piedra") {
-    console.log("piedra gana")
-}
+function computerPlays() {
+    let resultado = Math.floor(Math.random() * palabras.length);
+    return palabras[resultado]
 }
 
-singleRound("Papel",tijera)
+let scorePlayer= 0;
+let scoreComputer=0;
 
+const scoreFinal = function() {
+    if (scorePlayer > scoreComputer) {
+        console.log("HUMANO gana la partida")
+    } else if (scorePlayer<scoreComputer){
+        console.log("COMPUTADORA gana la partida")
+    } else {
+        console.log("El juego es un empate")
+    }
+}
+
+const playRound = function (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    if (playerSelection === computerSelection) {
+        return "empate"
+    }
+    else if (playerSelection === papel && computerSelection === "piedra") {
+        scorePlayer++
+        return "papel gana"
+        
+    }
+    else if (playerSelection === papel && computerSelection === "tijera") {
+        scoreComputer++
+        return "tijera gana"
+    }
+    else if (playerSelection === piedra && computerSelection === "tijera") {
+        scorePlayer ++
+        return "piedra gana"
+    }
+    else if (playerSelection === piedra && computerSelection === "papel") {
+        scoreComputer ++
+        return "papel gana"
+    }
+    else if (playerSelection === tijera && computerSelection === "papel") {
+        scorePlayer ++
+        return "tijera gana"
+    }
+    else if (playerSelection === tijera && computerSelection === "piedra") {
+        scoreComputer ++
+        return "piedra gana"
+    }
+}
+
+const playerSelection = "piedra";
+const computerSelection = computerPlays();
+console.log(playRound(playerSelection, computerSelection));
+console.log(scorePlayer)
+console.log(scoreComputer)
+scoreFinal()

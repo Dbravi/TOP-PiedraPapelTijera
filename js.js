624c788f5ed839713bd4860aea0199b38025496e
Dbravi <damianbravi@gmail.com>
2022-05-17 15:20:11 -0300

    Add some html for GUI

--- js.js
+++ js.js
@@ -13,59 +13,58 @@ let scoreComputer = 0;
 
 const scoreFinal = function () {
     if (scorePlayer > scoreComputer) {
-        console.log("HUMANO gana la partida")
+        document.getElementById("rfinal").innerHTML = "RESULTADO FINAL: HUMANO gana la partida"
     } else if (scorePlayer < scoreComputer) {
-        console.log("COMPUTADORA gana la partida")
+        document.getElementById("rfinal").innerHTML = "RESULTADO FINAL: COMPUTADORA gana la partida"
     } else {
-        console.log("El juego es un empate")
+        document.getElementById("rfinal").innerHTML = "RESULTADO FINAL: EMPATE!"
     }
 }
 
 const playRound = function (playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
-    let i=0
+    let i=1
     do {
+        // playerSelection = prompt("piedra, papel o tijera?");
         computerSelection = computerPlays()
-        console.log (`Round ${i}, Humano elige ${playerSelection} y Computadora elige ${computerSelection}`)
+        console.log (`Round ${i}: Humano elige ${playerSelection} y Computadora elige ${computerSelection}`)
         if (playerSelection === computerSelection) {
-            console.log("Empate, punto para cada uno")
+            console.log("Empate, punto para cada uno!")
             scorePlayer++
             scoreComputer++
         }
         else if (playerSelection === papel && computerSelection === "piedra") {
             scorePlayer++
-            console.log("Humano gana round, papel gana a piedra")
+            console.log("Humano gana round, papel le gana a piedra!")
         }
         else if (playerSelection === papel && computerSelection === "tijera") {
             scoreComputer++
-            console.log("Computadora gana round, tijera gana a papel")
+            console.log("Computadora gana round, tijera le gana a papel!")
         }
         else if (playerSelection === piedra && computerSelection === "tijera") {
             scorePlayer++
-            console.log("Humano gana round, piedra gana a tijera")
+            console.log("Humano gana round, piedra le gana a tijera!")
         }
         else if (playerSelection === piedra && computerSelection === "papel") {
             scoreComputer++
-            console.log("Computadora gana round, papel gana a piedra")
+            console.log("Computadora gana round, papel le gana a piedra!")
         }
         else if (playerSelection === tijera && computerSelection === "papel") {
             scorePlayer++
-            console.log("Humano gana round, tijera gana a papel")
+            console.log("Humano gana round, tijera le gana a papel!")
         }
         else if (playerSelection === tijera && computerSelection === "piedra") {
             scoreComputer++
-            console.log("Computadora gana round, piedra gana a tijera")
+            console.log("Computadora gana round, piedra le piedgana a tijera!")
         }
         console.log(`Humano Score parcial: ${scorePlayer}`)
         console.log(`Computadora Score parcial: ${scoreComputer}`)
         i++
     }
-    while(i<5);
+    while(i<6);
 }
 
-const playerSelection = "piedra";
+const playerSelection="piedra";
 const computerSelection = computerPlays();
 playRound(playerSelection, computerSelection);
-console.log(`Humano Score final: ${scorePlayer}`)
-console.log(`Computadora Score final: ${scoreComputer}`)
 scoreFinal()

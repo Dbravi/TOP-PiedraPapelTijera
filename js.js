27c728171f357b00c3e56e21bdd5de58c4cdb5c8
Dbravi <damianbravi@gmail.com>
2022-05-17 13:01:32 -0300

    add single round tentative solution

--- js.js
+++ js.js
@@ -1,14 +1,41 @@
-//input: none
-//solution(steps):
-//1.
-//2.
-//3.
-//output: console.log(random Rock’, ‘Paper’ or ‘Scissors)
-
 const palabras= ["rock","papper", "scissors"]
-function getRandomInt() {
+function computerPlay() {
     let resultado= Math.floor(Math.random() * palabras.length);
     console.log(palabras[resultado]) 
   }
 
-getRandomInt()
+// computerPlay()
+
+//if - else if 3 options
+//
+//
+
+const paper="paper"
+const rock="rock"
+const scissors="scissors"
+
+const singleRound= function (playerSelection, computerSelection) {
+if (playerSelection=== computerSelection) {
+    return console.log ("empate")
+}
+else if(playerSelection === paper && computerSelection === "rock") {
+   console.log("paper wins")
+}
+else if(playerSelection === paper && computerSelection === "scissors") {
+    console.log("scissors wins")
+}
+else if(playerSelection === rock && computerSelection === "scissors") {
+    console.log("rock wins")
+}
+else if(playerSelection === rock && computerSelection === "paper") {
+    console.log("scissors wins")
+}
+else if(playerSelection === scissors && computerSelection === "paper") {
+    console.log("scissors wins")
+}
+else if(playerSelection === scissors && computerSelection === "rock") {
+    console.log("rock wins")
+}
+}
+
+singleRound(rock,scissors)

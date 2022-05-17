68c6505661bc24341ff30da36212d6d452972689
Dbravi <damianbravi@gmail.com>
2022-05-17 13:27:18 -0300

    Agrego validación a lowerCase en caso de input incorrecto

--- js.js
+++ js.js
@@ -1,4 +1,8 @@
-const palabras= ["rock","papper", "scissors"]
+const papel="papel"
+const piedra="piedra"
+const tijera="tijera"
+
+const palabras= ["piedra","papel", "tijera"]
 function computerPlay() {
     let resultado= Math.floor(Math.random() * palabras.length);
     console.log(palabras[resultado]) 
@@ -6,36 +10,30 @@ function computerPlay() {
 
 // computerPlay()
 
-//if - else if 3 options
-//
-//
-
-const paper="paper"
-const rock="rock"
-const scissors="scissors"
-
 const singleRound= function (playerSelection, computerSelection) {
+    playerSelection=playerSelection.toLowerCase()
 if (playerSelection=== computerSelection) {
     return console.log ("empate")
 }
-else if(playerSelection === paper && computerSelection === "rock") {
-   console.log("paper wins")
+else if(playerSelection === papel && computerSelection === "piedra") {
+   console.log("papel gana")
 }
-else if(playerSelection === paper && computerSelection === "scissors") {
-    console.log("scissors wins")
+else if(playerSelection === papel && computerSelection === "tijera") {
+    console.log("tijera gana")
 }
-else if(playerSelection === rock && computerSelection === "scissors") {
-    console.log("rock wins")
+else if(playerSelection === piedra && computerSelection === "tijera") {
+    console.log("piedra gana")
 }
-else if(playerSelection === rock && computerSelection === "paper") {
-    console.log("scissors wins")
+else if(playerSelection === piedra && computerSelection === "papel") {
+    console.log("tijera gana")
 }
-else if(playerSelection === scissors && computerSelection === "paper") {
-    console.log("scissors wins")
+else if(playerSelection === tijera && computerSelection === "papel") {
+    console.log("tijera gana")
 }
-else if(playerSelection === scissors && computerSelection === "rock") {
-    console.log("rock wins")
+else if(playerSelection === tijera && computerSelection === "piedra") {
+    console.log("piedra gana")
 }
 }
 
-singleRound(rock,scissors)
+singleRound("Papel",tijera)
+
